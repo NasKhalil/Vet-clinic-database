62e73c777b928d22a446cdc44ca00186e7bf7d62
Khalil <khalilnasri92@gmail.com>
2022-04-25 14:48:36 +0100

    update table, add data, add transacions and write queries

--- schema.sql
+++ schema.sql
@@ -1,10 +1,15 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id integer PRIMARY KEY NOT NULL,
+    id integer GENERATED ALWAYS AS IDENTITY,
     animal_name varchar(100),
     date_of_birth date NOT NULL,
     escape_attempts integer,
     neutered boolean NOT NULL,
-    weight_per_kg decimal NOT NULL
+    weight_per_kg decimal NOT NULL,
+    PRIMARY KEY(id)
 );
+
+/* update table, add new column "species" */
+
+ALTER TABLE animals ADD COLUMN species varchar(50);
